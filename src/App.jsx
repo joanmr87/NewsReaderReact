8899891f97bb4feb6e50f7d8b0f1ec1ca5812b4c
joanMRomero <joanmr87@gmail.com>
2021-07-30 10:05:12 -0300

    inicio proyecto React News, importo json para pruebas

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import '../src/styles/App.css';
+import newsData from '../src/data/newsdata.json';
+
+function App() {
+  //console.log(newsData.articles)
+  const { articles } = newsData;
+  return (
+    <div className="App">
+     <h1>InfoNews</h1>
+     
+    </div>
+  );
+}
+
+export default App;
